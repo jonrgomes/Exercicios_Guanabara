bf1ac1cfa4980c98b0016201d53d04fce0d02aea
jonrgomes <jonrgomes@gmail.com>
2026-07-17 19:56:54 -0300

    curso_em_video

---
+++ Exer_008_Mundo_-1.py
@@ -0,0 +1,5 @@
+metros = float(input('Digite uma medida em metros:'))
+cen = metros * 100
+mm = cen * 10
+print(f'O {metros} inserido tem {cen:.0f}cm!')
+print(f'O {metros} inserido tem {mm:.0f}mm!')
